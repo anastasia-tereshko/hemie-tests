2b94f0cbd526380c9612b69324234bfb3dc0a15d
anastasia-tereshko <anastasia.tereshko@solveit.dev>
2025-05-08 21:19:33 +0300

    fix attempt hope2

--- login_test.js
+++ login_test.js
@@ -1,15 +1,6 @@
 console.log("������ Тест стартовал...");
-
-const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
-const path = require('path');
-
-// Указываем путь к ChromeDriver вручную
-//const service = new chrome.ServiceBuilder('/usr/local/bin/chromedriver').build();
-
-// Устанавливаем службу ChromeDriver
-//chrome.setDefaultService(service);
-
+const { Builder, By, until } = require('selenium-webdriver');
 (async function hemieLogin() {
   let options = new chrome.Options();
   options.addArguments('--headless');
@@ -20,25 +11,34 @@ const path = require('path');
     .forBrowser('chrome')
     .setChromeOptions(options)
     .build();
-
- // try {
+    
+  try {
     await driver.get('https://hemie.se/');
 
+    // Wait and click the "Log in" button (in the upper right corner)
     await driver.wait(until.elementLocated(By.xpath('//*[contains(@class, "header-btn primary")]')), 20000);
     await driver.findElement(By.xpath('//*[contains(@class, "header-btn primary-button")]')).click();
 
+    // Waiting for login form to load
     await driver.wait(until.elementLocated(By.css('input[name="email"]')), 20000);
+
+    // Enter email
     await driver.findElement(By.css('input[name="email"]')).sendKeys('anastasia.tereshko+32@solveit.dev');
+
+    // Enter password
     await driver.findElement(By.css('input[name="password"]')).sendKeys('Hejsan123!');
+
+    // Click the "Log in" button
     await driver.findElement(By.css('button[type="submit"]')).click();
 
+    // Wait until some element appears indicating a successful login. (greeting)
     await driver.wait(until.elementLocated(By.xpath('//*[contains(@class, "greeting")]')), 20000);
 
     console.log('✅ Login successful!');
-  //} //catch (error) {
-    //console.error('❌ Error while logging in:', error);
-  //} finally {
-  //  await driver.sleep(3000);
+  } catch (error) {
+    console.error('❌ Error while logging in:', error);
+  } finally {
+    await driver.sleep(3000); // wait 3 sec to see the result
     await driver.quit();
-  //}
+  }
 })();
