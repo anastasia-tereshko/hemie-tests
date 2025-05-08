1d0cc2cc2cc6986b8e277ddc28f136d2f3dbdee5
anastasia-tereshko <anastasia.tereshko@solveit.dev>
2025-05-08 20:05:48 +0300

    Fix workflows and comment headless

--- login_test.js
+++ login_test.js
@@ -3,9 +3,9 @@ const chrome = require('selenium-webdriver/chrome');
 const { Builder, By, until } = require('selenium-webdriver');
 (async function hemieLogin() {
   let options = new chrome.Options();
-  options.addArguments('--headless');
-  options.addArguments('--no-sandbox');
-  options.addArguments('--disable-dev-shm-usage');
+  //options.addArguments('--headless');
+  //options.addArguments('--no-sandbox');
+  //options.addArguments('--disable-dev-shm-usage');
 
   let driver = await new Builder()
     .forBrowser('chrome')
@@ -16,11 +16,11 @@ const { Builder, By, until } = require('selenium-webdriver');
     await driver.get('https://hemie.se/');
 
     // Wait and click the "Log in" button (in the upper right corner)
-    await driver.wait(until.elementLocated(By.xpath('//*[contains(@class, "header-btn primary")]')), 10000);
+    await driver.wait(until.elementLocated(By.xpath('//*[contains(@class, "header-btn primary")]')), 20000);
     await driver.findElement(By.xpath('//*[contains(@class, "header-btn primary-button")]')).click();
 
     // Waiting for login form to load
-    await driver.wait(until.elementLocated(By.css('input[name="email"]')), 10000);
+    await driver.wait(until.elementLocated(By.css('input[name="email"]')), 20000);
 
     // Enter email
     await driver.findElement(By.css('input[name="email"]')).sendKeys('anastasia.tereshko+32@solveit.dev');
@@ -32,7 +32,7 @@ const { Builder, By, until } = require('selenium-webdriver');
     await driver.findElement(By.css('button[type="submit"]')).click();
 
     // Wait until some element appears indicating a successful login. (greeting)
-    await driver.wait(until.elementLocated(By.xpath('//*[contains(@class, "greeting")]')), 10000);
+    await driver.wait(until.elementLocated(By.xpath('//*[contains(@class, "greeting")]')), 20000);
 
     console.log('✅ Login successful!');
   } catch (error) {
